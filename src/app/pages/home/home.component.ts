e5aea6a2ac056ea7014659206ef158b3d5463cb4
clara23 <clarahuanaco20@gmail.com>
2023-01-04 14:27:57 -0500

    actualisacion final

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit  } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 
-export class HomeComponent  {
+export class HomeComponent implements OnInit {
+ 
+  public formlogin!:FormGroup;
+  correo!:string;
 
+  constructor(private formBuilder:FormBuilder){
+
+  }
+
+  ngOnInit():void{
+    this.formlogin = this.formBuilder.group({
+      nombre:['',
+        [
+          Validators.required,
+          Validators.minLength(10) 
+        ]
+      ],
+      correo:['', 
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ]
+  })
+  this.loadAPI()
+}
+loadAPI():any{
+  const response = {
+    nombre:'mabel huanaco ',
+    correo: 'clahuanaco23@gmail.com', 
+  };
+
+  this.formlogin.patchValue(response);
+}
+
+send(): any{
+  console.log(this.formlogin.value)
+}
   
 }

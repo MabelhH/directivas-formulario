e414c1067310c2eab352a023bbe33a57c8fdd430
clara23 <clarahuanaco20@gmail.com>
2023-01-04 13:43:11 -0500

    tercera actulisacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -13,7 +12,6 @@ import { PagesModule } from './pages/pages.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     PagesModule
   ],
   providers: [],
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
-import{ ReactiveFormsModule} from '@angular/forms';
+import{FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
@@ -10,7 +10,8 @@ import{ ReactiveFormsModule} from '@angular/forms';
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
     
   ],
   exports:[
